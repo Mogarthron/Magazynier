78e875e8b63a965f27c8a046807475b3d78bc978
Mogarthron <witluczynski@gmial.com>
2024-03-22 15:24:13 +0100

    Flask server

--- app.py
+++ app.py
@@ -1,41 +1,77 @@
 from flask import Flask, render_template, request, url_for, flash, redirect
 from Analiza_pianek import Podsumowanie_analizy_pianek
-from Analiza_pianek.instrukcje_zamawiana import instrukcja_zamawiania_pianpol
+from Analiza_pianek.instrukcje_zamawiana import instrukcja_zamawiania_pianpol as izp
 from datetime import datetime as dt, timedelta
-arp = Podsumowanie_analizy_pianek(instrukcja_zamawiania_pianpol)
+# arp = Podsumowanie_analizy_pianek(instrukcja_zamawiania_pianpol)
 
 app = Flask(__name__)
+pap = Podsumowanie_analizy_pianek(izp)
 
 @app.route("/")
 def index():
-
-    return render_template("index.html", tables=[arp.Tabela_podsumowania_analizy.to_html(index=False, classes='table table-striped table-hover', header="true")])   
+    return "ddd"
+    # return render_template("index.html", tables=[arp.Tabela_podsumowania_analizy.to_html(index=False, classes='table table-striped table-hover', header="true")])   
 
 @app.route("/kalendarz_dostaw")
 def kalendarz_dostaw():
 
-    kal_dos = {dt(2024,3,15): {"DOSTAWCA": "PIANPOL",
-                          "NR PARTII": "08/01",
-                          "NR DOS": "24/0216",
-                          "MODELE": "AVA",
-                          "OBJ": ["width: 55%;", 55]},
-                dt(2024,3,22): {"DOSTAWCA": "VITA",
+    kal_dos = {
+        # dt(2024,3,15): [{"DOSTAWCA": "PIANPOL",
+        #                   "NR PARTII": "08/01",
+        #                   "NR DOS": "24/0216",
+        #                   "MODELE": "AVA",
+        #                   "OBJ": ["width: 42%;", 42]},],
+                dt(2024,3,22): [{"DOSTAWCA": "VITA",
                           "NR PARTII": "09/01",
                           "NR DOS": "24/0299",
-                          "MODELE": "STO, HOR, BLOK T-35",
+                          "MODELE": "STO, HOR, BLOK T-30",
                           "OBJ": ["width: 99%;", 99]},
-                dt(2024,3,20): {"DOSTAWCA": "PIANPOL",
+                        #   {"DOSTAWCA": "PIANPOL",
+                        #   "NR PARTII": "08/01",
+                        #   "NR DOS": "24/0216",
+                        #   "MODELE": "MAX, OXY",
+                        #   "OBJ": ["width: 25%;", 25]}
+                          ],
+                dt(2024,3,27): [{"DOSTAWCA": "PIANPOL",
                           "NR PARTII": "08/01",
                           "NR DOS": "24/0216",
                           "MODELE": "MAX, OXY",
-                          "OBJ": ["width: 25%;", 25]},
+                          "OBJ": ["width: 40%;", 40]},],
+                dt(2024,4,4): [{"DOSTAWCA": "OWATY",
+                          "NR PARTII": "",
+                          "NR DOS": "",
+                          "MODELE": "REV, SAM",
+                          "OBJ": []}],
+                dt(2024,4,5): [{"DOSTAWCA": "PIANPOL",
+                          "NR PARTII": "10/01",
+                          "NR DOS": "24/0299",
+                          "MODELE": "AVA, ELI, REV",
+                          "OBJ": ["width: 74%;", 74]},
+
+                          {"DOSTAWCA": "OWATY",
+                          "NR PARTII": "",
+                          "NR DOS": "",
+                          "MODELE": "REV, SAM",
+                          "OBJ": []}],
+            
+                dt(2024,4,12): [{"DOSTAWCA": "CIECH",
+                          "NR PARTII": "11/01",
+                          "NR DOS": "24/0327",
+                          "MODELE": "SAM, DIV, GRE, OVA, CAL, KEL",
+                          "OBJ": ["width: 83%;", 83]},],
+                dt(2024,4,19): [{"DOSTAWCA": "PIANPOL",
+                          "NR PARTII": "12/01",
+                          "NR DOS": "24/0347",
+                          "MODELE": "REV, AVA",
+                          "OBJ": ["width: 78%;", 78]},],            
                                               }
     
     lista_dni = list()
     start_mc = 3
-    start_d = 11
+    start_d = 18
+    tygodni_do_przodu = 7
 
-    for i in range(7):
+    for i in range(tygodni_do_przodu):
         pierwszy_dzien = dt(2024,start_mc,start_d)+timedelta(7*i)
         tydzien = dt.isocalendar(pierwszy_dzien).week
         # lista_dni.append([tydzien] + [(pierwszy_dzien + timedelta(x)).strftime("%d-%b") for x in range(5)])
@@ -44,7 +80,10 @@ def kalendarz_dostaw():
     
     return render_template("kalendarz_dostaw.html", lista_dni=lista_dni, kal_dos=kal_dos)
 
+@app.route("/analiza_pianek")
+def analiza_pianek():    
 
+    return render_template("analiza_pianek.html", pap=pap)
 
 if __name__ == "__main__":
     app.run(debug=True, host="127.0.0.1", port=5000)
