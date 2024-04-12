f64cdbf592af61cb414e427029aa77126fd8d320
Mogarthron <witluczynski@gmial.com>
2024-04-12 15:28:33 +0200

    init

--- run.py
+++ run.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     if len(sys.argv) == 1:
         app.run(debug=True, host="127.0.0.1", port=5000)
     else:
-        app.run(debug=True, host=sys.argv[1], port=sys.argv[2])
+        app.run(debug=False, host=sys.argv[1], port=sys.argv[2])
