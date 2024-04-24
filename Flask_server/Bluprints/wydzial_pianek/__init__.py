9de9c9aa2434c97c6580a6ed2216e33d2e51f3cb
Mogarthron <witluczynski@gmial.com>
2024-04-24 11:51:59 +0200

    Flask server

--- Flask_server/Bluprints/wydzial_pianek/__init__.py
+++ Flask_server/Bluprints/wydzial_pianek/__init__.py
@@ -1,7 +1,7 @@
 from flask import Blueprint 
 
 
-wydzial_pianek = Blueprint("wydzial_pianek", __name__, template_folder="templates/wydzial_pianek")
+wydzial_pianek = Blueprint("wydzial_pianek", __name__, template_folder="templates/wydzial_pianek", static_folder="static")
 
 
 from ..wydzial_pianek.routes import *
--- Flask_server/Bluprints/wydzial_pianek/funkcje_pomocnicze.py
+++ Flask_server/Bluprints/wydzial_pianek/funkcje_pomocnicze.py
@@ -1,5 +1,12 @@
 from Modele_db.modele_db import ZAM_PIANKI, RAPORT_KJ_DO_DOSTAWY_PIANEK as kj, KOMPLETY_PIANEK, session
 from sqlalchemy import select, text
+import json 
+import os
+
+
+# with open("linki.json", "r", encoding="UTF8") as f:
+    # rysunki_dir = json.load(f)["rysunki_dokumentacja"]
+rysunki_dir = "Flask_server/Bluprints/wydzial_pianek/static/RYSUNKI/"
 
 def raport_jakosciowy_dane(id):
     bryla_jakosc = select(ZAM_PIANKI.kod ,ZAM_PIANKI.model, ZAM_PIANKI.nr_kompletacji, ZAM_PIANKI.opis, ZAM_PIANKI.ile_zam, ZAM_PIANKI.zam1, ZAM_PIANKI.zam2).where(
@@ -15,13 +22,24 @@ def raport_jakosciowy_dane(id):
     uw = [list(x) for x in session.execute(select(kj.nr_pianki, kj.uwaga_wysokosc, kj.data_dodania).where(kj.model == model, kj.bryla_gen == bryla_gen, kj.blad_dopuszczalny_wysokosc==True, kj.uwaga_wysokosc != "")).all()]
     us = [list(x) for x in session.execute(select(kj.nr_pianki, kj.uwaga_szerokosc, kj.data_dodania).where(kj.model == model, kj.bryla_gen == bryla_gen, kj.blad_dopuszczalny_szerokosc==True, kj.uwaga_szerokosc != "")).all()]
     ud = [list(x) for x in session.execute(select(kj.nr_pianki, kj.uwaga_dlugosc, kj.data_dodania).where(kj.model == model, kj.bryla_gen == bryla_gen, kj.blad_dopuszczalny_dlugosc==True, kj.uwaga_dlugosc != "")).all()]
-
+    print(len(uw), len(us), len(ud))
 
     for r in tabelka_kj:
+
+        gif_dir = rysunki_dir + r[4][0] + "/"
+        # gif_dir = "/" + r[4][0] + "/"
+        # gif_dir = r[4][0] + "/"
+
+        try:
+            gif_dir = "/" + r[4][0] + "/"+[x for x in os.listdir(gif_dir) if r[4] in x][0]
+        except:
+            gif_dir = "BRAK"
+
+        r.append(gif_dir)
         r.append("")
         r.append("")
         r.append("")
-                # print(r)
+                
 
     for r in tabelka_kj:
         for d in ud:
