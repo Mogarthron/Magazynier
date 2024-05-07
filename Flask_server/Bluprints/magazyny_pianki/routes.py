4c962984021445c47df3f592ae7bd75b7ff341c3
Mogarthron <witluczynski@gmial.com>
2024-05-07 14:41:33 +0200

    Flask server

--- Flask_server/Bluprints/magazyny_pianki/routes.py
+++ Flask_server/Bluprints/magazyny_pianki/routes.py
@@ -2,10 +2,18 @@ from flask import render_template, request, redirect, url_for
 from ..magazyny_pianki import magazyny_pianki
 
 
+magazyny = {
+    "MAGAZYN 1 ANTRESOLA 1": "",
+    "MAGAZYN 1 ANTRESOLA 2": "",
+    "MAGAZYN 2": "",
+    "MAGAZYN 10": "",
+    "MAGAZYN 11": ""
+            }
+
 @magazyny_pianki.route("/")
 def index():
 
     magazyn_2 = list()
 
 
-    return render_template("magazyny_pianki.html", title="Magazyn 2")
+    return render_template("magazyny_pianki.html", title="MAGAZYNY PIANEK", magazyny=magazyny)
