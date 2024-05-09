12b453de2cb8cdce7192880e510eb39f43097f8f
Mogarthron <witluczynski@gmial.com>
2024-05-09 10:35:47 +0200

    Flask server

--- Flask_server/Bluprints/wydzial_pianek/routes.py
+++ Flask_server/Bluprints/wydzial_pianek/routes.py
@@ -34,8 +34,10 @@ def dostawy_pianek():
    
     tabelka_dostawy_pianek = [
         #Data zamó[wienia], oczekiwnie na potwierdzenie, data potwierdzenia, data dostawy, nr_partii, nr_samochodu, status, obietosc
-        ["2024-04-05", "", "", "2024-05-10", "13/01, 14/01", "PIANPOL 10_24", "NIE POTWIERDZONY", f"{df[df.SAMOCHOD == 'PIANPOL 10_24'].OBJ.sum():.0f}"],
-        ["2024-03-27", "", "", "2024-05-17", "13/01", "VITA 8_24", "NIE POTWIERDZONY", f"{df[df.SAMOCHOD == 'VITA 8_24'].OBJ.sum():.0f}"],
+        ["2024-04-05", "", "", "2024-05-10", "13/01, 14/01", "PIANPOL 10_24", "POTWIERDZONY", f"{df[df.SAMOCHOD == 'PIANPOL 10_24'].OBJ.sum():.0f}"],
+        ["2024-03-27", "", "", "2024-05-17", "13/01", "VITA 8_24", "POTWIERDZONY", f"{df[df.SAMOCHOD == 'VITA 8_24'].OBJ.sum():.0f}"],
+        ["2024-05-08", "", "", "2024-06-07", "19/01", "PIANPOL 11_24", "NIE POTWIERDZONY", f"{df[df.SAMOCHOD == 'PIANPOL 11_24'].OBJ.sum():.0f}"],
+        ["2024-05-08", "", "", "2024-06-07", "19/02", "PIANPOL 12_24", "NIE POTWIERDZONY", f"{df[df.SAMOCHOD == 'PIANPOL 12_24'].OBJ.sum():.0f}"],
     ]
     
     
