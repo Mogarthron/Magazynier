de1615f02683dd931a75b645c1c7b5ac35cf5b6a
Mogarthron <witluczynski@gmial.com>
2024-05-21 09:11:33 +0200

    Flask server

--- Flask_server/Bluprints/analiza_pianek/routes.py
+++ Flask_server/Bluprints/analiza_pianek/routes.py
@@ -10,6 +10,8 @@ from ..analiza_pianek import analiza_pianek
 ard = {x.MODEL: x for x in izp}
 pap = Podsumowanie_analizy_pianek(izp)
 
+zam_pianki = dict()
+
 @analiza_pianek.route("/")
 def index():
     return render_template("analiza_pianek.html", title="Analiza pianek", pap=pap)
@@ -24,10 +26,27 @@ def dokumentacja_pianek_numer(numer):
 @analiza_pianek.route("/dodaj_pianki_bryla/<model>", methods=["GET", "POST"])
 def dodaj_pianki_bryla(model):
 
-    if request.method == "POST":
-        print({k:v for k,v in request.form.lists()})
-        print(request.form.getlist("ile"))
-        # print(request.form["dodajBryly"])
+    if request.method == "POST" and "dodajBryly" in request.form.keys():
+        bryly_do_zamowienia = {k:v for k,v in request.form.lists()}
+        zam_pianki[model] = {}
+        for i in range(len(bryly_do_zamowienia["bryla"])):
+            zam_pianki[model][f"{bryly_do_zamowienia['bryla'][i]}"] = float(bryly_do_zamowienia['ile'][i])
+
+        print(zam_pianki)
+
+    elif request.method == "POST" and "sprawdzObj" in request.form.keys():
+        _lista_korekty_zam = {k:v for k,v in request.form.lists()}
+        lista_korekty_zam = dict()
+        for i in range(len(_lista_korekty_zam["bryla"])):
+            lista_korekty_zam[f"{_lista_korekty_zam['bryla'][i]}"] = float(_lista_korekty_zam['ile'][i])
+        
+        # print(lista_korekty_zam)
+        cls = ard[model].Bryly_do_zamowienia(wszystkie_bryly=True, korekta_zam=lista_korekty_zam)[1]
+
+        _model = model + f" PIANPOL:{cls.pianpol_VOL:.0f}, VITA:{cls.vita_VOL:.0f}, CIECH:{cls.ciech_VOL:.0f}"
+        
+        return render_template("dodaj_pianki_bryla.html", title="Dodaj Bryły - " + model, model=_model, bryla=ard[model].Bryly_do_zamowienia(wszystkie_bryly=True, lista_korekty_zam=lista_korekty_zam))
+    
 
 
     return render_template("dodaj_pianki_bryla.html", title="Dodaj Bryły - " + model, model=model, bryla=ard[model].Bryly_do_zamowienia(wszystkie_bryly=True, lista_korekty_zam=True))
@@ -35,9 +54,13 @@ def dodaj_pianki_bryla(model):
 @analiza_pianek.route("/dodaj_pianki_model", methods=["GET", "POST"])
 def dodaj_pianki_model():
 
-    if request.method == "POST":
+    if request.method == "POST" and "przejdz_do_bryly" in request.form.keys():
     
         return redirect(url_for("analiza_pianek.dodaj_pianki_bryla", model=request.form["model"]))
+    
+    if request.method == "POST" and "wyczysc_bryly" in request.form.keys():
+    
+        zam_pianki = {}
 
     return render_template("dodaj_pianki_model.html", title="Dodaj Pianki", lista_modeli = list([x.MODEL for x in izp])) 
 
