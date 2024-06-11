483e9add20b75324bdea4f898ec5f8b76cbfa924
Mogarthron <witluczynski@gmial.com>
2024-06-11 15:07:16 +0200

    Flask server

--- Flask_server/Bluprints/wydzial_pianek/routes/plan_pracy.py
+++ Flask_server/Bluprints/wydzial_pianek/routes/plan_pracy.py
@@ -9,30 +9,30 @@ def plan_pracy():
     
     json_plan_pracy = list(map(lambda x: x.plan_pracy_to_json(), plan_pracy))
 
+    if request.method == "POST":
+        if "zakonczono" in list(request.form.keys())[0]:
+            print("zakonczono id", int(list(request.form.keys())[0].replace("zakonczono_", "")))
+            
+        if "edytuj" in list(request.form.keys())[0]:
+            print("edytuj id", int(list(request.form.keys())[0].replace("edytuj_", "")))
 
-    if request.method == "POST" and "zakonczono" in list(request.form.keys())[0]:
-        print("zakonczono id", int(list(request.form.keys())[0].replace("zakonczono_", "")))
-        
-    if request.method == "POST" and "edytuj" in list(request.form.keys())[0]:
-        print("edytuj id", int(list(request.form.keys())[0].replace("edytuj_", "")))
+        if "leniwa" == list(request.form.keys())[0].split("_")[0]:
+            print("leniwa id", int(list(request.form.keys())[0].replace("leniwa_", "")))
 
-    if request.method == "POST" and "leniwa" == list(request.form.keys())[0].split("_")[0]:
-        print("leniwa id", int(list(request.form.keys())[0].replace("leniwa_", "")))
+        if "leniwaSkos" == list(request.form.keys())[0].split("_")[0]:
+            print("leniwaSkos id", int(list(request.form.keys())[0].replace("leniwaSkos_", "")))
 
-    if request.method == "POST" and "leniwaSkos" == list(request.form.keys())[0].split("_")[0]:
-        print("leniwaSkos id", int(list(request.form.keys())[0].replace("leniwaSkos_", "")))
+        if "owatyWydane" in list(request.form.keys())[0].split("_")[0]:
+            print("owatyWydane id", int(list(request.form.keys())[0].replace("owatyWydane_", "")))
 
-    if request.method == "POST" and "owatyWydane" in list(request.form.keys())[0].split("_")[0]:
-        print("owatyWydane id", int(list(request.form.keys())[0].replace("owatyWydane_", "")))
+        if "owatyWyciete" in list(request.form.keys())[0].split("_")[0]:
+            print("owatyWyciete id", int(list(request.form.keys())[0].replace("owatyWyciete_", "")))
 
-    if request.method == "POST" and "owatyWyciete" in list(request.form.keys())[0].split("_")[0]:
-        print("owatyWyciete id", int(list(request.form.keys())[0].replace("owatyWyciete_", "")))
+        if "owatyKompletacja" in list(request.form.keys())[0].split("_")[0]:
+            print("owatyKompletacja id", int(list(request.form.keys())[0].replace("owatyKompletacja_", "")))
 
-    if request.method == "POST" and "owatyKompletacja" in list(request.form.keys())[0].split("_")[0]:
-        print("owatyKompletacja id", int(list(request.form.keys())[0].replace("owatyKompletacja_", "")))
-
-    if request.method == "POST" and "kj" in list(request.form.keys())[0].split("_")[0]:
-        # print("kj id", int(list(request.form.keys())[0].replace("kj_", "")))
-        return redirect(url_for("wydzial_pianek.raport_jakosciowy", id=int(list(request.form.keys())[0].replace("kj_", ""))))
+        if "kj" in list(request.form.keys())[0].split("_")[0]:
+            # print("kj id", int(list(request.form.keys())[0].replace("kj_", "")))
+            return redirect(url_for("wydzial_pianek.raport_jakosciowy", id=int(list(request.form.keys())[0].replace("kj_", ""))))
 
     return render_template("plan_pracy.html", title="PLAN PRACY", plan_pracy={"plan_pracy":json_plan_pracy})
