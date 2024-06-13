96536829ae7399b410017c4ec8ad10d6fb4e7f4c
Mogarthron <witluczynski@gmial.com>
2024-06-13 15:30:16 +0200

    Pianki

--- Pianki/Analiza_pianek/instrukcje_zamawiana.py
+++ Pianki/Analiza_pianek/instrukcje_zamawiana.py
@@ -4,8 +4,8 @@ from Pianki.Modele_pianek import *
 
 #INSTRUKCJA ZAM_NOWA_PIANPOL
 instrukcja_zamawiania_pianpol=[
-    Analiza_Rodziny(AVANT_P,1,1),
-    Analiza_Rodziny(ELIXIR_P,1,1),
+    Analiza_Rodziny(AVANT_P,1,11),
+    Analiza_Rodziny(ELIXIR_P,1,2),
     Analiza_Rodziny(STONE_P,1,1),
     Analiza_Rodziny(HORIZON_PV,1,1),
     Analiza_Rodziny(AMALFI_P,2,1),
@@ -13,21 +13,21 @@ instrukcja_zamawiania_pianpol=[
     Analiza_Rodziny(MAXWELL_P,2,2),
     Analiza_Rodziny(OXYGEN_P,2),
     Analiza_Rodziny(WILLOW_P,2,2),
-    Analiza_Rodziny(REVERSO_P,2,1),
-    Analiza_Rodziny(GOYA_P,3),
-    Analiza_Rodziny(DUO_P,3),
+    Analiza_Rodziny(REVERSO_P,2,2),
+    Analiza_Rodziny(GOYA_P,3,10),
+    Analiza_Rodziny(DUO_P,3,100),
     Analiza_Rodziny(HUDSON_P,3,2),
     Analiza_Rodziny(CALYPSO_P,3,2),
     Analiza_Rodziny(ONYX_P,3,2),
-    Analiza_Rodziny(RITZ_C,3),
+    Analiza_Rodziny(RITZ_C,3,10),
     Analiza_Rodziny(SAMOA_P,3,2),
-    Analiza_Rodziny(SPECTRA_C,3),
-    Analiza_Rodziny(COCO_P,4),
-    Analiza_Rodziny(GREY_C,4),
-    Analiza_Rodziny(OVAL_P,4),
-    Analiza_Rodziny(LENOX_P,4),
-    Analiza_Rodziny(UNO_P,4),
-    Analiza_Rodziny(KELLY_C,4), #POPRAWIC KLASE!!!!
+    Analiza_Rodziny(SPECTRA_C,3,2),
+    Analiza_Rodziny(COCO_P,4,100),
+    Analiza_Rodziny(GREY_C,4,100),
+    Analiza_Rodziny(OVAL_P,4,100),
+    Analiza_Rodziny(LENOX_P,4,100),
+    Analiza_Rodziny(UNO_P,4,100),
+    Analiza_Rodziny(KELLY_C,4,100), #POPRAWIC KLASE!!!!
     # Analiza_Rodziny(EXTREME,3), #ZROBIC KLASE
     # Analiza_Rodziny(MYSTIC,3), #NIE/ZROBIC KLASE?
 ]
