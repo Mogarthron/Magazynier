f96e56b159366ce9cbc78292489f68d22594cd13
Mogarthron <witluczynski@gmial.com>
2024-08-14 14:49:00 +0200

    Flask server

--- Flask_server/Bluprints/analiza_pianek/routes/analiza_modelu.py
+++ Flask_server/Bluprints/analiza_pianek/routes/analiza_modelu.py
@@ -1,7 +1,13 @@
 from ..routes import *
 
+
+
+
+import plotly
+import json
+
 @analiza_pianek.route("/analiza_modelu/<model>", methods=["GET", "POST"])
 def analiza_modelu(model):
     _model = pap[model]
 
-    return render_template("analiza_modelu.html", tabelka_modelu=_model.ar) 
+    return render_template("analiza_modelu.html", tabelka_modelu=_model.ar, graphJSON=json.dumps(_model.Wykres_obj(nazwa_modelu=False, show_fig=False), cls=plotly.utils.PlotlyJSONEncoder)) 
