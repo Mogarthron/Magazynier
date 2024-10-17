d9f6379d4fd17ff941b2ae7b66fcec32ea16381f
Mogarthron <witluczynski@gmial.com>
2024-10-17 12:08:17 +0200

    Flask server

--- Flask_server/Bluprints/analiza_pianek/routes/drukuj_raporty.py
+++ Flask_server/Bluprints/analiza_pianek/routes/drukuj_raporty.py
@@ -2,14 +2,26 @@ from ..routes import *
 from Pianki.Raporty_do_zamowien.raporty_do_zamowien import dane_zam_pianki_czasy, drukuj_raporty
 from flask import send_file
 
-@analiza_pianek.route("/drukuj_raporty_do_zamowien/<nr_tygodnia>/<zlecenie>/<zam>", methods=["GET", "POST"])
-def drukuj_raporty_do_zamowien(nr_tygodnia, zlecenie, zam):
+@analiza_pianek.route("/raporty_do_zamowien", methods=["GET", "POST"])
+def raporty_do_zamowien():
     
-    
+    if request.method == "POST":
+        nr_partii = request.form["nr_partii"]
+        zlecenie = request.form["typ_zlecenia"]
+        zam = request.form["zam"]
+        print(nr_partii, zlecenie, zam)
+        pozycje = dane_zam_pianki_czasy(nr_partii)
 
-    pozycje = dane_zam_pianki_czasy(int(nr_tygodnia))
+        print(pozycje)
+
+        drukuj_raporty(nr_partii, zlecenie=zlecenie, pozycje=pozycje, zam=zam)
+
+        return redirect(url_for('analiza_pianek.pobierz_raporty_do_zamowien', zlecenie=zlecenie, nr_partii=nr_partii.replace("/", "_")))    
+        
     
-    drukuj_raporty(nr_tygodnia, zlecenie=zlecenie, zam=zam, pozycje=pozycje)
-            
-    return send_file(f"../ZLECENIA_PROD/{zlecenie}/{nr_tygodnia} {zlecenie}.pdf")
+    return render_template("drukuj_raporty.html")
     
+@analiza_pianek.route("/pobierz_raporty_do_zamowien/<zlecenie>/<nr_partii>")
+def pobierz_raporty_do_zamowien(zlecenie, nr_partii):
+
+    return send_file(f"../ZLECENIA_PROD/{zlecenie}/{nr_partii} {zlecenie}.pdf")
