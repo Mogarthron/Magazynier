1ebe91e7df78536573655cb179c9a817fdc82394
Mogarthron <witluczynski@gmial.com>
2024-11-19 13:07:50 +0100

    Flask server

--- Flask_server/Bluprints/wydzial_pianek/routes/plan_pracy.py
+++ Flask_server/Bluprints/wydzial_pianek/routes/plan_pracy.py
@@ -26,21 +26,19 @@ def plan_pracy():
             
             return redirect(url_for("wydzial_pianek.plan_pracy", title="PLAN PRACY", plan_pracy=zaladuj_plan_pracy()))
             # return render_template("plan_pracy.html", title="PLAN PRACY", plan_pracy=zaladuj_plan_pracy())
-        
-        if "edytuj" in list(request.form.keys())[0]:
-            print("edytuj id", int(list(request.form.keys())[0].replace("edytuj_", "")))
+                       
 
-        if "leniwa" == list(request.form.keys())[0].split("_")[0]:
-            print("leniwa id", int(list(request.form.keys())[0].replace("leniwa_", "")))
+        if "zatwierdzDostawe" in list(request.form.keys()):
+            print(list(request.form.keys()))
+            print("zatwierdzDostawe id", list(request.form.keys())[0].split("_")[-1])
+            print("Status:", request.form[list(request.form.keys())[0]])
 
-        if "leniwaSkos" == list(request.form.keys())[0].split("_")[0]:
-            print("leniwaSkos id", int(list(request.form.keys())[0].replace("leniwaSkos_", "")))
+            id_zam_pianki = int(list(request.form.keys())[0].split("_")[-1])
+            poz = session.query(ZAM_PIANKI).get(id_zam_pianki)           
+            poz.status_kompletacja = request.form[list(request.form.keys())[0]]
+            session.commit()
 
-        if "owatyWydane" in list(request.form.keys())[0].split("_")[0]:
-            print("owatyWydane id", int(list(request.form.keys())[0].replace("owatyWydane_", "")))
-
-        if "owatyWyciete" in list(request.form.keys())[0].split("_")[0]:
-            print("owatyWyciete id", int(list(request.form.keys())[0].replace("owatyWyciete_", "")))
+            return redirect(url_for("wydzial_pianek.plan_pracy", title="PLAN PRACY", plan_pracy=zaladuj_plan_pracy()))
 
         if "owatyKompletacja" in list(request.form.keys())[0].split("_")[0]:
             print("owatyKompletacja id", int(list(request.form.keys())[0].replace("owatyKompletacja_", "")))
@@ -52,8 +50,8 @@ def plan_pracy():
 
             return redirect(url_for("wydzial_pianek.plan_pracy", title="PLAN PRACY", plan_pracy=zaladuj_plan_pracy()))
 
-        if "kj" in list(request.form.keys())[0].split("_")[0]:
-            # print("kj id", int(list(request.form.keys())[0].replace("kj_", "")))
-            return redirect(url_for("wydzial_pianek.raport_jakosciowy", id=int(list(request.form.keys())[0].replace("kj_", ""))))
+        # if "kj" in list(request.form.keys())[0].split("_")[0]:
+        #     # print("kj id", int(list(request.form.keys())[0].replace("kj_", "")))
+        #     return redirect(url_for("wydzial_pianek.raport_jakosciowy", id=int(list(request.form.keys())[0].replace("kj_", ""))))
 
     return render_template("plan_pracy.html", title="PLAN PRACY", plan_pracy={"plan_pracy":json_plan_pracy})
