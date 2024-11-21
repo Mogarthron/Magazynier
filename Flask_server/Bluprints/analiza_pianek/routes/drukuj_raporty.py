2b5b9d4b8041e74dd6cd5adf88dc5f6027870c7d
Mogarthron <witluczynski@gmial.com>
2024-11-21 13:06:18 +0100

    Flask server

--- Flask_server/Bluprints/analiza_pianek/routes/drukuj_raporty.py
+++ Flask_server/Bluprints/analiza_pianek/routes/drukuj_raporty.py
@@ -1,5 +1,5 @@
 from ..routes import *
-from Pianki.Raporty_do_zamowien.raporty_do_zamowien import dane_zam_pianki_czasy, drukuj_raporty
+from Pianki.Raporty_do_zamowien.raporty_do_zamowien import dane_zam_pianki_czasy, drukuj_raporty, drukuj_raporty_xlsx
 from flask import send_file
 
 @analiza_pianek.route("/raporty_do_zamowien", methods=["GET", "POST"])
@@ -9,19 +9,33 @@ def raporty_do_zamowien():
         nr_partii = request.form["nr_partii"]
         zlecenie = request.form["typ_zlecenia"]
         zam = request.form["zam"]
-        print(nr_partii, zlecenie, zam)
+        dostawca = request.form["dostawca"]
+        print(nr_partii, zlecenie, zam, dostawca)
         pozycje = dane_zam_pianki_czasy(nr_partii)
 
-        print(pozycje)
+        # print(pozycje)
+        
+        if zlecenie not in ["WOZKI DO DOSTAWY", "RAPORT MEMORY", "CZASY PROCESOW"]:
 
-        drukuj_raporty(nr_partii, zlecenie=zlecenie, pozycje=pozycje, zam=zam)
+            drukuj_raporty(nr_partii, zlecenie=zlecenie, pozycje=pozycje, zam=zam)
 
-        return redirect(url_for('analiza_pianek.pobierz_raporty_do_zamowien', zlecenie=zlecenie, nr_partii=nr_partii.replace("/", "_")))    
+            return redirect(url_for('analiza_pianek.pobierz_raporty_do_zamowien', zlecenie=zlecenie, nr_partii=nr_partii.replace("/", "_")))
+        
+        else:
+            drukuj_raporty_xlsx(nr_partii, zlecenie, dostawca)
+
+            return redirect(url_for('analiza_pianek.pobierz_raporty_do_zamowien', zlecenie=zlecenie, nr_partii=nr_partii.replace("/", "_"), dostawca=dostawca))
+
+            
         
     
     return render_template("drukuj_raporty.html")
     
-@analiza_pianek.route("/pobierz_raporty_do_zamowien/<zlecenie>/<nr_partii>")
-def pobierz_raporty_do_zamowien(zlecenie, nr_partii):
-
-    return send_file(f"../ZLECENIA_PROD/{zlecenie}/{nr_partii} {zlecenie}.pdf")
+@analiza_pianek.route("/pobierz_raporty_do_zamowien/<zlecenie>/<nr_partii>", defaults={'dostawca': None})
+@analiza_pianek.route("/pobierz_raporty_do_zamowien/<zlecenie>/<nr_partii>/<dostawca>")
+def pobierz_raporty_do_zamowien(zlecenie, nr_partii, dostawca=None):
+    if dostawca:
+        print(dostawca)
+        return send_file(f"../ZLECENIA_PROD/{zlecenie}/{nr_partii}/{dostawca}_{zlecenie}.xlsx")
+    else:
+        return send_file(f"../ZLECENIA_PROD/{zlecenie}/{nr_partii} {zlecenie}.pdf")
