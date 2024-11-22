6d783ce6acce0f239d16cfeec4e576a227638f4e
Mogarthron <witluczynski@gmial.com>
2024-11-22 11:01:21 +0100

    Flask server

--- Flask_server/Bluprints/analiza_pianek/routes/drukuj_raporty.py
+++ Flask_server/Bluprints/analiza_pianek/routes/drukuj_raporty.py
@@ -1,5 +1,5 @@
 from ..routes import *
-from Pianki.Raporty_do_zamowien.raporty_do_zamowien import dane_zam_pianki_czasy, drukuj_raporty, drukuj_raporty_xlsx
+from Pianki.Raporty_do_zamowien.raporty_do_zamowien import dane_zam_pianki_czasy, drukuj_raporty, drukuj_raporty_xlsx, naklejki_na_paczki_pianek
 from flask import send_file
 
 @analiza_pianek.route("/raporty_do_zamowien", methods=["GET", "POST"])
@@ -15,12 +15,24 @@ def raporty_do_zamowien():
 
         # print(pozycje)
         
-        if zlecenie not in ["WOZKI DO DOSTAWY", "RAPORT MEMORY", "CZASY PROCESOW"]:
+        if zlecenie not in ["WOZKI DO DOSTAWY", "RAPORT MEMORY", "CZASY PROCESOW", "NAKLEJKI OWATY", "NAKLEJKI PIANKI"]:
 
             drukuj_raporty(nr_partii, zlecenie=zlecenie, pozycje=pozycje, zam=zam)
 
             return redirect(url_for('analiza_pianek.pobierz_raporty_do_zamowien', zlecenie=zlecenie, nr_partii=nr_partii.replace("/", "_")))
         
+        elif zlecenie == "NAKLEJKI OWATY":
+             
+            naklejki_na_paczki_pianek(nr_partii,zam,True)
+
+            return redirect(url_for('analiza_pianek.pobierz_raporty_do_zamowien', zlecenie=zlecenie, nr_partii=nr_partii.replace("/", "_")))
+
+        elif zlecenie == "NAKLEJKI PIANKI":
+             
+            naklejki_na_paczki_pianek(nr_partii,zam,False)
+
+            return redirect(url_for('analiza_pianek.pobierz_raporty_do_zamowien', zlecenie=zlecenie, nr_partii=nr_partii.replace("/", "_")))
+
         else:
             drukuj_raporty_xlsx(nr_partii, zlecenie, dostawca)
 
@@ -34,8 +46,15 @@ def raporty_do_zamowien():
 @analiza_pianek.route("/pobierz_raporty_do_zamowien/<zlecenie>/<nr_partii>", defaults={'dostawca': None})
 @analiza_pianek.route("/pobierz_raporty_do_zamowien/<zlecenie>/<nr_partii>/<dostawca>")
 def pobierz_raporty_do_zamowien(zlecenie, nr_partii, dostawca=None):
+    print("send file:", zlecenie, nr_partii, dostawca)
     if dostawca:
-        print(dostawca)
         return send_file(f"../ZLECENIA_PROD/{zlecenie}/{nr_partii}/{dostawca}_{zlecenie}.xlsx")
+    
+    elif zlecenie == "NAKLEJKI OWATY":        
+        return send_file(f"../ZLECENIA_PROD/NAKLEJKI OWATY/{nr_partii}.pdf")
+    
+    elif zlecenie == "NAKLEJKI PIANKI":        
+        return send_file(f"../ZLECENIA_PROD/NAKLEJKI PIANKI/{nr_partii}.pdf")
+    
     else:
         return send_file(f"../ZLECENIA_PROD/{zlecenie}/{nr_partii} {zlecenie}.pdf")
